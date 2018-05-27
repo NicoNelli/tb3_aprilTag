5a395fe6b88a4ad567fc009c2f1bc198ca8aafed
NicoNelli <nicolobastianelli@gmail.com>
2018-05-27 14:34:34 +0200

    adapt source file

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 echo "setting up apriltags_ws..."
 source /opt/ros/kinetic/setup.bash
-source ~/tb3_aprilTag/catkin_ws/devel/setup.bash
+source /home/nicolo/Desktop/VisionSystem/tb3_aprilTag/catkin_ws/devel/setup.bash
 #for opencv-3.1 with pkg_config
 PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
 export PKG_CONFIG_PATH
